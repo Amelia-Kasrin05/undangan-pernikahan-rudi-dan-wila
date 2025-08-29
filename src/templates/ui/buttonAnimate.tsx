d48f968587fe1a5a36f00880d60214beb7f840b7
Your Name <ameliakasrin05@gmail.com>
2025-08-29 12:17:31 +0700

    buttonAnimate

--- src/templates/ui/buttonAnimate.tsx
+++ src/templates/ui/buttonAnimate.tsx
@@ -6,6 +6,7 @@ export default function ButtonAnimate({
   img,
   children,
   onClick,
+  className,
 }: {
   button: {
     ref: React.RefObject<HTMLButtonElement>;
@@ -15,20 +16,17 @@ export default function ButtonAnimate({
   img?: string;
   children: React.ReactNode;
   onClick?: () => void;
+  className?: string;
 }) {
   return (
     <button
       ref={button.ref}
       onClick={onClick}
-      className={`relative bg-blue-400 px-4 py-[10px] text-white rounded-full flex items-center justify-center gap-2 self-center hover:bg-blue-400/80 ${
-        windowWidth < 500 && "text-[13px]"
-      } cursor-pointer z-20 overflow-hidden`}
+      className={`relative bg-blue-400 px-4 py-[10px] text-white rounded-full flex items-center justify-center gap-2 self-center hover:bg-blue-400/80 ${windowWidth < 500 && "text-[13px]"} cursor-pointer z-20 overflow-hidden ${
+        className || ""
+      }`}
     >
-      <motion.div
-        animate={button.isVisible ? { scaleX: 0 } : { scaleX: 1 }}
-        transition={{ duration: 0.5 }}
-        className="absolute top-0 left-0 w-full h-full rounded-full bg-blue-400 origin-right"
-      />
+      <motion.div animate={button.isVisible ? { scaleX: 0 } : { scaleX: 1 }} transition={{ duration: 0.5 }} className="absolute top-0 left-0 w-full h-full rounded-full bg-blue-400 origin-right" />
       {img && <img src={img} alt="" className="max-w-[20px]" />}
       {children}
     </button>
