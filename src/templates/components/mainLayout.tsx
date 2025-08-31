22020987865f0ea406e8899e7665e7ad155d17e0
Your Name <ameliakasrin05@gmail.com>
2025-08-31 14:28:28 +0700

    border

--- src/templates/components/mainLayout.tsx
+++ src/templates/components/mainLayout.tsx
@@ -1,5 +1,3 @@
-
-
 export default function MainLayout({
   children,
   height = "min-h-screen",
@@ -14,42 +12,20 @@ export default function MainLayout({
   flower?: boolean;
 }) {
   return (
-    <section
-      className={`relative w-full ${height} flex flex-col justify-center items-center ${backgroundColor} p-32 px-10 ${className}`}
-    >
+    <section className={`relative w-full ${height} flex flex-col justify-center items-center ${backgroundColor} p-32 px-10 ${className}`}>
       {flower && (
         <div className={`w-full h-full overflow-hidden absolute z-0`}>
-          <img
-            src="/images/bercak2.png"
-            alt=""
-            className="absolute -left-56 top-1/2 opacity-20"
-          />
-          <img
-            src="/images/bercak2.png"
-            alt=""
-            className="absolute -right-48 top-[15%] opacity-20"
-          />
-          <img
-            src="/images/bunga3.png"
-            alt=""
-            className="absolute -top-9 -left-5 w-[60dvw] max-w-64 z-0"
-          />
-          <img
-            src="/images/bunga4.png"
-            alt=""
-            className="absolute -top-[8rem] -right-10 w-[60dvw] max-w-64 z-0 rotate-[110deg]"
-          />
+          {/* Background decorative elements */}
+          <img src="/images/bercak2.png" alt="" className="absolute -left-56 top-1/2 opacity-20" />
+          <img src="/images/bercak2.png" alt="" className="absolute -right-48 top-[15%] opacity-20" />
 
-          <img
-            src="/images/bunga3.png"
-            alt=""
-            className="absolute -bottom-9 -right-5 w-[60dvw] max-w-64 z-0 rotate-180"
-          />
-          <img
-            src="/images/bunga4.png"
-            alt=""
-            className="absolute -bottom-[7.5rem] -left-10 w-[60dvw] max-w-64 z-0 rotate-[-65deg]"
-          />
+          {/* Corner flowers - Top corners */}
+          <img src="/images/bunga3.png" alt="" className="absolute top-0 left-0 w-[50dvw] max-w-56 z-0" />
+          <img src="/images/bunga3.png" alt="" className="absolute top-0 right-0 w-[50dvw] max-w-56 z-0 -scale-x-100" />
+
+          {/* Corner flowers - Bottom corners */}
+          <img src="/images/bunga3.png" alt="" className="absolute bottom-0 left-0 w-[50dvw] max-w-56 z-0 -scale-y-100" />
+          <img src="/images/bunga3.png" alt="" className="absolute bottom-0 right-0 w-[50dvw] max-w-56 z-0 -scale-y-100 -scale-x-100" />
         </div>
       )}
 
